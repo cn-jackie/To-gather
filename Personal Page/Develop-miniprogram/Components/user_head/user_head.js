2c338a32d9a2bae3464b2b152670926e6bddaab6
cn-jackie <cn-jackie@outlook.com>
2022-10-09 20:01:45 +0800

    Add files via upload

---
+++ Personal Page/Develop-miniprogram/Components/user_head/user_head.js
@@ -0,0 +1,29 @@
+// Components/user_head/user_head.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  },
+
+  lifetimes: {
+    attached: function() {
+      
+    }
+  }
+})
